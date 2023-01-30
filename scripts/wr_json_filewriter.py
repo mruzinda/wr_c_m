2fccead5a757fdd40c37e1982e56d7c94b8a96b1
sonata <ddeboer@berkeley.edu>
2023-01-30 20:55:43 +0000

    Added a script that just writes polled data from WR-LENs to JSON files. Also modified redis_monitor script towrite data to separate JSON files depending on WR-LEN.

--- scripts/wr_json_filewriter.py
+++ scripts/wr_json_filewriter.py
@@ -1,12 +1,11 @@
 '''
 Simple script to poll WR-LEN endpoints by hostname and
-write stats to redis and also dynamic stats to JSON file
+write dynamic stats to JSON file
 '''
 
 def main():
     import socket
     import argparse
-    import redis
     import time
     import datetime
     import os
@@ -23,8 +22,6 @@ def main():
                     help='WR hosts to poll. Default: Node{0..32}wr')
     parser.add_argument('-t', dest='polltime', type=float, default=30,
                     help='Minimum time between polling cycles in seconds. Default: 30s')
-#    r = redis.Redis('redishost')
-    r = redis.Redis()
 
     args = parser.parse_args()
     
@@ -34,20 +31,15 @@ def main():
         hosts = args.hosts
 
     print('Begging WR-LEN status polling. Hosts are:')
-    script_redis_key = "status:script:%s:%s" % (hostname, __file__)
     wr_devices = {}
     count = 0
     while(True):
-        r.set(script_redis_key, "alive", ex=60)
-        r.hmset("version:%s:%s" % (__package__, os.path.basename(__file__)), {"version":__version__, "timestamp":datetime.datetime.now().isoformat()})
         start_time = time.time()
         count += 1
         for host in hosts:
-        
             # JSON filename - WR-LEN dynamic stats are written to this file
             json_filename = "dynamic_wrlen_stats_%s.json" % host.replace(".","-")
-        
-            hash_key = 'status:wr:%s' % host
+            
             if host not in wr_devices.keys():
                 try:
                     ip = socket.gethostbyname(host)
@@ -103,16 +95,10 @@ def main():
                     print("JSON file output:")
                     print(read_json_file)
                 
-                r.hmset(hash_key, stats)
-                # Delete old keys in case there is some weird stale stuff
-                old_keys = [k for k in r.hkeys(hash_key) if k not in stats.keys()]
-                if len(old_keys) > 0:
-                    r.hdel(hash_key, *old_keys)
             except:
                 print("Error while polling %s" % host)
         extra_wait = args.polltime - (time.time() - start_time)
         while extra_wait > 0:
-            r.set(script_redis_key, "alive", ex=60)
             time.sleep(min(extra_wait, 60))
             extra_wait -= 60
 
